a82ccfacded247659723b99afeb86c96dbc898c8
James Turnbull <james@lovedthanlost.net>
2012-07-28 14:52:23 -0700

    Added some more tests

--- spec/statum_spec.rb
+++ spec/statum_spec.rb
@@ -8,4 +8,46 @@ describe Statum::Application do
       last_response.should be_ok
     end
   end
+
+  describe "GET '/user/login'" do
+    it "should return the login page." do
+      get '/user/login'
+      last_response.should be_ok
+    end
+  end
+
+  describe "GET '/user/logout'" do
+    it "should return the logout page." do
+      get '/user/logout'
+      last_response.should be_ok
+    end
+  end
+
+  describe "GET '/user/create'" do
+    it "should return the user creation page." do
+      get '/user/create'
+      last_response.should be_ok
+    end
+  end
+
+  describe "GET '/user/list'" do
+    it "should return the user listing page." do
+      get '/user/list'
+      last_response.should be_ok
+    end
+  end
+
+  describe "GET '/user/delete'" do
+    it "should return the user deletion page." do
+      get '/user/delete'
+      last_response.should be_ok
+    end
+  end
+
+  describe "GET '/status/list'" do
+    it "should return the status listing page." do
+      get '/status/list'
+      last_response.should be_ok
+    end
+  end
 end
